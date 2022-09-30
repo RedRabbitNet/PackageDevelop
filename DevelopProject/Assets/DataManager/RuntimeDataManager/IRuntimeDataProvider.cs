96df4eb0063f6d0b08b52536e5d0fc3241fb28d2
RedRabbitNet <redrabbitnet@gmail.com>
2022-09-30 18:16:09 +0900

    RuntimeDataManagerの仮実装

---
+++ DevelopProject/Assets/DataManager/RuntimeDataManager/IRuntimeDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// ここに実装する機能を宣言する
+/// </summary>
+public interface IRuntimeDataProvider
+{
+    
+}
+public interface IRuntimeDataProvider<DataType> : IRuntimeDataProvider
+{
+    List<DataType> DataList { get; set; }
+}
---
+++ DevelopProject/Assets/DataManager/RuntimeDataManager/RuntimeDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// ここに定義を記述する
+/// 機能を追加する場合、IRuntimeDataProviderにも追加する
+/// ProviderはDataManagerでしか生成しない
+/// </summary>
+public class RuntimeDataProvider<DataType> : IRuntimeDataProvider<DataType>
+{
+    public List<DataType> DataList { get; set; }
+    private string dataPath;
+
+    public RuntimeDataProvider(string path)
+    {
+        dataPath = path;
+    }
+}
